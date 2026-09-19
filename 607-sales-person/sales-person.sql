43acf265a24796a9f81780cffac83a415b0ec4af
Aditi Bhatt <aditibhatt1809@gmail.com>
2026-09-19 22:42:42 +0530

    Time: 1361 ms (92.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select s.name 
+from salesperson s
+where not exists (
+    select 1
+    from orders o join company c
+    on o.com_id =c.com_id
+    where o.sales_id = s.sales_id
+    and c.name ='RED'
+);
